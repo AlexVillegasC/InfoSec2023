0f45acaa377d1d4d2a8e79c8bf39b69c483920bc
Dian7875 <dian7875.a@gmail.com>
2023-08-20 00:16:02 -0600

    Correccion de EX Server-Zip

--- Labs/Lab1/AdrianAguilar-504420813/EX1.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const archiver = require('archiver');
-var output = fs.createWriteStream('./'+'Lab-Comprimir'+'.zip');//Direccion de guardado
-var archive = archiver('zip')
-archive.pipe(output);
-
-archive.directory('./mis-archivos',"mis-archivos");//Ruta y nombre de carpeta
-
-archive.finalize();
--- Labs/Lab1/AdrianAguilar-504420813/server-zip-[EX].js
+++ Labs/Lab1/AdrianAguilar-504420813/server-zip-[EX].js
@@ -1,3 +1,13 @@
 // Ejercicio:
 // Vaya a npm y busque una libreria que sirva para comprimir archivos .zip
 // Comprima todos los archivos .txt que estan en la carpeta mis-archivos, genera un "UNA.zip" dentro del folder mis-archivos
+const fs = require('fs');
+const archiver = require('archiver');
+var output = fs.createWriteStream('mis-archivos/'+'UNA'+'.zip');//define ruta y nombre para el o los archvios a comprimir
+var archive = archiver('zip')//establece el tipo de archivo
+archive.pipe(output);
+
+//archive.directory('./mis-archivos',"mis-archivos");//comprime por ruta, Error al comprimir archvios de una carpeta y guardar el zip creado en esa misma carpeta
+archive.append(fs.createReadStream('./mis-archivos/j-type-327.txt'),{name:'j-type-327.txt'})//comprime por archivos solamente el archivo seleccionado, ruta, nombre post compresion
+archive.append(fs.createReadStream('./mis-archivos/a-wing.txt'),{name:'a-wing.txt'})
+archive.finalize();
