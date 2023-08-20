e7039ab1d521bd20675d8273c5c1844a40a30b3b
Dian7875 <dian7875.a@gmail.com>
2023-08-19 23:52:11 -0600

    cometarios

--- Labs/Lab1/AdrianAguilar-504420813/EX1.js
+++ Labs/Lab1/AdrianAguilar-504420813/EX1.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const archiver = require('archiver');
-var output = fs.createWriteStream('./'+'Lab-Comprimir'+'.zip');
+var output = fs.createWriteStream('./'+'Lab-Comprimir'+'.zip');//Direccion de guardado
 var archive = archiver('zip')
 archive.pipe(output);
 
-archive.directory('./mis-archivos',"mis-archivos");
+archive.directory('./mis-archivos',"mis-archivos");//Ruta y nombre de carpeta
 
 archive.finalize();
