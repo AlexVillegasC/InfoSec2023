7a4c55d5b5d39413baea0a23d3d358ef872e5410
Kennetejaen7 <kenneth.jaen.paniagua@est.una.ac.cr>
2023-08-25 21:08:42 -0600

    lab1_kenneth_jaen_504410318

--- Labs/Lab1/KennethJaen-504410318/server-zip-[EX].js
+++ Labs/Lab1/KennethJaen-504410318/server-zip-[EX].js
@@ -1,3 +1,16 @@
 // Ejercicio:
 // Vaya a npm y busque una libreria que sirva para comprimir archivos .zip
 // Comprima todos los archivos .txt que estan en la carpeta mis-archivos, genera un "UNA.zip" dentro del folder mis-archivos
+
+// creating archives
+var zip = new AdmZip();
+
+// add file directly
+var content = "inner content of the file";
+zip.addFile("test.txt", Buffer.from(content, "utf8"), "entry comment goes here");
+// add local file
+zip.addLocalFile("/home/me/some_picture.png");
+// get everything as a buffer
+var willSendthis = zip.toBuffer();
+// or write everything to disk
+zip.writeZip(/*target file name*/ "/home/me/files.zip");
