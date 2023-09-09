850bce8412031fc08f32c75fd9563bddcfc289b7
TonyMoraga <tonm618@gmail.com>
2023-09-09 09:13:42 -0600

    Cambios

---
+++ Labs/Lab1/AnthonyMoraga-504270353/server-unzip-[EX].js
@@ -0,0 +1,4 @@
+// Vaya a npm y busque una libreria que sirva para descomprimir archivos .zip
+// Haga unzip de "UNA.zip"
+
+
---
+++ Labs/Lab1/AnthonyMoraga-504270353/streams/streams.js
@@ -0,0 +1,14 @@
+var fs = require("fs")
+
+var readerStream = fs.createReadStream(`${__dirname}../../mis-archivos/a-wing.txt`);
+
+var writerStream = fs.createWriteStream(`${__dirname}../../mis-archivos/j-type-327.txt`);
+
+// Se lee el input.txt y se agrega la data a output.txt
+readerStream.pipe(writerStream);
+
+// Salvar el pipe TODO.
+// Ejercicio: trate de salvar el texto en un nuevo archivo
+
+// fin de codigo
+console.log("Program Ended");
