fa5a009c5ec6b910a4e7bacb74f0f8580890df93
FiorellaBonilla <fiorella.bonilla.gonzalez@est.una.ac.cr>
2023-09-09 09:57:32 -0600

    lab5-fiorella

--- Labs/Lab5 - TDD/FiorellaBonilla-504390363/server.js
+++ Labs/Lab5 - TDD/FiorellaBonilla-504390363/server.js
@@ -2,6 +2,7 @@
 // npm install para descargar los paquetes...
 
 // libreriuas
+var validation = require('unalib')();
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -17,6 +18,7 @@ io.on('connection', function(socket){
   // si se escucha "chat message"
   socket.on('Evento-Mensaje-Server', function(msg){
     // volvemos a emitir el mismo mensaje
+    msg = validation.validateMessage(msg);
     io.emit('Evento-Mensaje-Server', msg);
   });
 });
