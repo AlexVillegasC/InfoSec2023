4cebae67c77fc773b2ccf611e1e64c450a8a4f20
Kennetejaen7 <kenneth.jaen.paniagua@est.una.ac.cr>
2023-09-09 20:22:51 -0600

    lab1-2-3

--- Labs/Lab1/KennethJaen-504410318/server-unzip-[EX].js
+++ Labs/Lab1/KennethJaen-504410318/server-unzip-[EX].js
@@ -1,23 +1,14 @@
 // Vaya a npm y busque una libreria que sirva para descomprimir archivos .zip
 // Haga unzip de "UNA.zip"
 
+const decompress = require("decompress"); 
 
-var AdmZip = require("adm-zip");
-
-// reading archives
-var zip = new AdmZip("./my_file.zip");
-var zipEntries = zip.getEntries(); // an array of ZipEntry records
-
-zipEntries.forEach(function (zipEntry) {
-    console.log(zipEntry.toString()); // outputs zip entries information
-    if (zipEntry.entryName == "my_file.txt") {
-        console.log(zipEntry.getData().toString("utf8"));
-    }
-});
-// outputs the content of some_folder/my_file.txt
-console.log(zip.readAsText("some_folder/my_file.txt"));
-// extracts the specified file to the specified location
-zip.extractEntryTo(/*entry name*/ "some_folder/my_file.txt", /*target path*/ "/home/me/tempfolder", /*maintainEntryPath*/ false, /*overwrite*/ true);
-// extracts everything
-zip.extractAllTo(/*target path*/ "/home/me/zipcontent/", /*overwrite*/ true);
+decompress("UNA.zip", "unzip-UNA") //Archivo a descomprimir con parametros, nombre del archivo seguido de nombre de la carpeta en donde se va a extraer
+  .then((files) => {
+    console.log(files);
+  })
+//!!clausula de error en caso de proceso fallido por diversas razones
+  .catch((error) => {
+    console.log(error);
+  });
 
--- Labs/Lab1/KennethJaen-504410318/server-zip-[EX].js
+++ Labs/Lab1/KennethJaen-504410318/server-zip-[EX].js
@@ -2,15 +2,13 @@
 // Vaya a npm y busque una libreria que sirva para comprimir archivos .zip
 // Comprima todos los archivos .txt que estan en la carpeta mis-archivos, genera un "UNA.zip" dentro del folder mis-archivos
 
-// creating archives
-var zip = new AdmZip();
+const fs = require('fs');
+const archiver = require('archiver');
+var output = fs.createWriteStream('mis-archivos/'+'UNA'+'.zip');//define ruta y nombre para el o los archvios a comprimir
+var archive = archiver('zip')//establece el tipo de archivo
+archive.pipe(output);
 
-// add file directly
-var content = "inner content of the file";
-zip.addFile("test.txt", Buffer.from(content, "utf8"), "entry comment goes here");
-// add local file
-zip.addLocalFile("/home/me/some_picture.png");
-// get everything as a buffer
-var willSendthis = zip.toBuffer();
-// or write everything to disk
-zip.writeZip(/*target file name*/ "/home/me/files.zip");
+//archive.directory('./mis-archivos',"mis-archivos");//comprime por ruta, Error al comprimir archvios de una carpeta y guardar el zip creado en esa misma carpeta
+archive.append(fs.createReadStream('./mis-archivos/j-type-327.txt'),{name:'j-type-327.txt'})//comprime por archivos solamente el archivo seleccionado, ruta, nombre post compresion
+archive.append(fs.createReadStream('./mis-archivos/a-wing.txt'),{name:'a-wing.txt'})
+archive.finalize();
