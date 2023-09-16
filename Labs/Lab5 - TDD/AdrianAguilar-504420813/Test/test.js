35fbcce59bfd62933b0b7f34f100401ec9042d99
Dian7875 <dian7875.a@gmail.com>
2023-09-16 08:54:23 -0600

    test url imagen, telefono invalido

--- Labs/Lab5 - TDD/AdrianAguilar-504420813/Test/test.js
+++ Labs/Lab5 - TDD/AdrianAguilar-504420813/Test/test.js
@@ -14,5 +14,34 @@ describe('unalib', function () {
             // esta es la comprobacion (o "afirmacion"). Si es falso, 
             assert.equal(val.is_valid_phone('6196-8116'), true);
         });
+
+        it('Debería rechazar un número de teléfono incorrecto', function () {
+            assert.equal(val.is_valid_phone('xxxxx'), false);
+          });
+        });
+      
+        describe('unalib', function () {
+          it('Debería validar una URL de imagen correcta', function () {
+            assert.equal(val.is_valid_url_image('https://example.com/image.jpg'), true);
+          });
+      
+          it('Debería rechazar una URL incorrecta', function () {
+            assert.equal(val.is_valid_url_image('not_an_image'), false);
+          });
+        });
+      
+        describe('unalib', function () {
+          it('Debería validar una URL de video de YouTube correcta', function () {
+            assert.equal(val.is_valid_yt_video('https://www.youtube.com/watch?v=videoId'), true);
+          });
+      
+          it('Debería rechazar una URL incorrecta', function () {
+            assert.equal(val.is_valid_yt_video('not_a_youtube_video'), false);
+          });
+        });
+      
+       
+      
+          
+
     });
-});
