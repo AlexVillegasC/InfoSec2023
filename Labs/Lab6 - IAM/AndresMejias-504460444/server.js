79529322210581aa1ee44fec63e646ca92efd30b
anerthy <112020687+anerthy@users.noreply.github.com>
2023-09-23 11:18:04 -0600

    se configura el server.js

--- Labs/Lab6 - IAM/AndresMejias-504460444/server.js
+++ Labs/Lab6 - IAM/AndresMejias-504460444/server.js
@@ -1,32 +1,29 @@
 "use strict";
 
 // Imports
+var unalibValidator = require('./unalib');
 const express = require("express");
 const session = require("express-session");
 const ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
-const { auth } = require('express-openid-connect');
-const { requiresAuth } = require('express-openid-connect');
-var cons = require('consolidate');
-var path = require('path');
-let app = express();
+const { auth, requiresAuth } = require('express-openid-connect');
+const cons = require('consolidate');
+const path = require('path');
 
 // Globals
-const OKTA_ISSUER_URI = "https://dev-gic3zvhz5ffpf7pf.us.auth0.com";
+const OKTA_ISSUER_URI = "https://dev-gic3zvhz5ffpf7pf.us.auth0.com/";
 const OKTA_CLIENT_ID = "y4KBoZFEUWRe3VxKa1Qx9IwJyGOJkict";
-const OKTA_CLIENT_SECRET = "wJRdZNuHXGq3ygycoPxmKU-amknlAl_NNb3GvF9BYL_NZ6ICyp5XsNEhoAi6s9yV";
+const OKTA_CLIENT_SECRET = "YOUR_SECRET_HERE"; // Remember to hide this
 const REDIRECT_URI = "http://localhost:3000/unaChat";
 const PORT = process.env.PORT || "3000";
-const SECRET = "hjsadfghjakshdfg87sd8f76s8d7f68s7f632342ug44gg423636346f"; // Dejar el secret así como está.
+const SECRET = "wJRdZNuHXGq3ygycoPxmKU-amknlAl_NNb3GvF9BYL_NZ6ICyp5XsNEhoAi6s9yV"; // Remember to hide this
 
-//  Esto se los dará Okta.
-// es el signing key
 const config = {
   authRequired: false,
   auth0Logout: true,
   secret: SECRET,
   baseURL: 'http://localhost:3000',
-  clientID: 'y4KBoZFEUWRe3VxKa1Qx9IwJyGOJkict',
-  issuerBaseURL: 'https://dev-gic3zvhz5ffpf7pf.us.auth0.com'
+  clientID: OKTA_CLIENT_ID,
+  issuerBaseURL: OKTA_ISSUER_URI
 };
 
 let oidc = new ExpressOIDC({
@@ -34,22 +31,23 @@ let oidc = new ExpressOIDC({
   client_id: OKTA_CLIENT_ID,
   client_secret: OKTA_CLIENT_SECRET,
   redirect_uri: REDIRECT_URI,
-  routes: { callback: { defaultRedirect: "http://localhost:3000/dashboard" } },
+  routes: { callback: { defaultRedirect: REDIRECT_URI } },
   scope: 'openid profile'
 });
+
+let app = express();
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
 // MVC View Setup
-app.engine('html', cons.swig)
+app.engine('html', cons.swig);
 app.set('views', path.join(__dirname, 'views'));
 app.set('models', path.join(__dirname, 'models'));
 app.set('view engine', 'html');
 
 // App middleware
 app.use("/static", express.static("static"));
-
 app.use(session({
   cookie: { httpOnly: true },
   secret: SECRET
@@ -63,12 +61,9 @@ app.get("/", (req, res) => {
 });
 
 app.get("/dashboard", requiresAuth(), (req, res) => {
-  // if(req.oidc.isAuthenticated())
-  // {
   var payload = Buffer.from(req.appSession.id_token.split('.')[1], 'base64').toString('utf-8');
   const userInfo = JSON.parse(payload);
   res.render("dashboard", { user: userInfo });
-  //}
 });
 
 app.get("/unaChat", requiresAuth(), (req, res) => {
@@ -80,11 +75,41 @@ app.get("/unaChat", requiresAuth(), (req, res) => {
 const openIdClient = require('openid-client');
 openIdClient.Issuer.defaultHttpOptions.timeout = 20000;
 
+var http = require('http').Server(app);
+//var io = require('socket.io')(http);
+var io = require('socket.io')(http, {
+  cors: {
+    origin: "http://localhost:3000", // Allow your express server's origin
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
+
+var chatPort = 3001; // separate port for chat
+
+// escuchar una conexion por socket
+io.on('connection', function (socket) {
+  // si se escucha "chat message"
+  socket.on('Evento-Mensaje-Server', function (msg) {
+    // Validamos el mensaje
+    msg = unalibValidator.validateMessage(msg);
+    // volvemos a emitir el mismo mensaje
+    io.emit('Evento-Mensaje-Server', msg);
+  });
+});
+
+
+
 oidc.on("ready", () => {
-  console.log("Server running on port: " + PORT);
+  console.log("Auth server running on port: " + PORT);
   app.listen(parseInt(PORT));
+  http.listen(chatPort, () => {
+    console.log('Chat server running on port: ' + chatPort);
+  });
 });
 
 oidc.on("error", err => {
   console.error(err);
 });
+
+
