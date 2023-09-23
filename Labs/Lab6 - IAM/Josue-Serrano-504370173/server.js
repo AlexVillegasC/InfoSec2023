f32db2995f0e559d98dfe4259c83892b8f80209b
504370173 <serranorosales9@gmail.com>
2023-09-23 09:59:22 -0600

    okta-login

--- Labs/Lab6 - IAM/Josue-Serrano-504370173/server.js
+++ Labs/Lab6 - IAM/Josue-Serrano-504370173/server.js
@@ -11,20 +11,21 @@ var path = require('path');
 let app = express();
 
 // Globals
-const OKTA_ISSUER_URI = "https://una-infosec.us.auth0.com/"
-const OKTA_CLIENT_ID = "mlIokKRjb5CGf8FbKpDIOKE36e7BjDLA";
-const OKTA_CLIENT_SECRET = "h8KznysLFpC2QHJHwTb_GDE1cnIesddtvURO-Yns_DQEYIJVG33QdeGOa8Bq7aWr";
+const OKTA_ISSUER_URI = "https://dev-infos.us.auth0.com"
+const OKTA_CLIENT_ID = "D897GzeZKq3IxzCWW1HzAr8KnXgrzGZR";
+const OKTA_CLIENT_SECRET = "b3OsCIBjiOCPV1PtvPJniy9VCldFOfo8_dnAHffKVhYf_yKGf7iursPJJIGl0rtg";
 const REDIRECT_URI = "http://localhost:3000/dashboard";
 const PORT = process.env.PORT || "3000";
-const SECRET = "hjsadfghjakshdfg87sd8f76s8d7f68s7f632342ug44gg423636346f";
+const SECRET = "hjsadfghjakshdfg87sd8f76s8d7f68s7f632342ug44gg423636346f"; // Dejar el secret así como está.
 
+//  Esto se los dará Okta.
 const config = {
   authRequired: false,
   auth0Logout: true,
   secret: SECRET,
   baseURL: 'http://localhost:3000',
-  clientID: 'mlIokKRjb5CGf8FbKpDIOKE36e7BjDLA',
-  issuerBaseURL: 'https://una-infosec.us.auth0.com'
+  clientID: 'D897GzeZKq3IxzCWW1HzAr8KnXgrzGZR',
+  issuerBaseURL: 'https://dev-infos.us.auth0.com'
 };
 
 let oidc = new ExpressOIDC({
@@ -48,26 +49,31 @@ app.set('view engine', 'html');
 // App middleware
 app.use("/static", express.static("static"));
 
-// app.use(session({
-//   cookie: { httpOnly: true },
-//   secret: SECRET
-// }));
+app.use(session({
+  cookie: { httpOnly: true },
+  secret: SECRET,
+  resave: false,            // Configuración de resave
+  saveUninitialized: true   // Configuración de saveUninitialized
+}));
 
 // App routes
 app.use(oidc.router);
 
 app.get("/",  (req, res) => {
-  res.render("index");
-  //res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  res.render("index");  
 });
 
 app.get("/dashboard", requiresAuth() ,(req, res) => {  
-  // if(req.oidc.isAuthenticated())
-  // {
+   if(req.oidc.isAuthenticated())
+   {
     var payload = Buffer.from(req.appSession.id_token.split('.')[1], 'base64').toString('utf-8');
     const userInfo = JSON.parse(payload);
     res.render("dashboard", { user: userInfo });
-  //}
+  }
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
 });
 
 const openIdClient = require('openid-client');
